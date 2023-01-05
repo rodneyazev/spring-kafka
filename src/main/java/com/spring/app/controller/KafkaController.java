be572bdd3d1bf6b88ff9d9f42d2e2f7285364475
rodneyazev <rodney.n.azevedo@gmail.com>
2023-01-05 11:22:25 -0300

    spring-kafka

---
+++ src/main/java/com/spring/app/controller/KafkaController.java
@@ -0,0 +1,23 @@
+package com.spring.app.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.app.Producer;
+
+@RestController
+@RequestMapping("/")
+public class KafkaController {
+	
+	@Autowired
+	Producer producer;
+	
+	@PostMapping("/post")
+	public void sendMessage(@RequestBody String msg) {
+		producer.publishTopic(msg);
+	}
+	
+}
