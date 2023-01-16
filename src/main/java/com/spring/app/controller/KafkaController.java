45c165df51b9bfa5d5e7f1a05d3799d630d7ecfa
rodneyazev <rodney.n.azevedo@gmail.com>
2023-01-16 12:31:20 -0300

    spring-kafka

--- src/main/java/com/spring/app/controller/KafkaController.java
+++ src/main/java/com/spring/app/controller/KafkaController.java
@@ -1,6 +1,8 @@
 package com.spring.app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
